83a3d647b9594c012da3aef6b4a6fa0ea8a185e9
Steve Ryan <steven.ryan@gmail.com>
2015-10-27 13:55:17 -0400

    Customized my config.h files

--- config.h
+++ config.h
@@ -1,2 +1,7 @@
 //here's a generic configuration header
 //v1.2 NEWER VERSION!
+
+//now I'll add some custom lines into my config file- I want them to stay in my local repository and not get
+//pushed to BlinkFork
+//(although sometimes I might!  If I worked on a new feature, for instance, that necessitated an addition to config.h)
+//Damn, this gets confusing!
